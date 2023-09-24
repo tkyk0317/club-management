fc988d85895c950055b4121ffa21eac615e7598c
takayuki tanaka <takayuki.1981.0317@gmail.com>
2023-09-24 09:32:22 +0900

    フロント部分のテストを追加

---
+++ frontend/src/__tests__/components/todo.test.tsx
@@ -0,0 +1,70 @@
+import Todo from "@app/components/Todo";
+import { render, fireEvent, screen } from "@testing-library/react";
+import axios from 'axios';
+
+describe("Todo表示テスト", () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+    vi.resetAllMocks();
+  });
+
+  test("取得成功", async () => {
+    // mock
+    vi.mock('axios')
+    axios.get.mockResolvedValue({
+      status: 200,
+      data: [{
+        id: 1,
+        content: "テストTodo",
+        created_at: "2023-01-01 00:00:00",
+        updated_at: "2023-01-01 00:00:00",
+      }]
+    })
+
+    // レンダー
+    render(<Todo />);
+
+    // 期待値確認
+    expect(axios.get).toHaveBeenCalledTimes(1)
+  });
+});
+
+describe("Todo更新テスト", () => {
+  afterEach(() => {
+    vi.clearAllMocks();
+    vi.resetAllMocks();
+  });
+
+  test("更新成功", async () => {
+    // mock
+    vi.mock('axios')
+    axios.put.mockResolvedValue({
+      status: 200,
+      data: {
+        id: 1,
+        content: "テストTodo更新",
+        created_at: "2023-01-01 00:00:00",
+        updated_at: "2023-01-01 00:00:00",
+      }
+    })
+    axios.get.mockResolvedValue({
+      status: 200,
+      data: [{
+        id: 1,
+        content: "テストTodo",
+        created_at: "2023-01-01 00:00:00",
+        updated_at: "2023-01-01 00:00:00",
+      }]
+    })
+
+    // レンダー
+    const { container } = render(<Todo />);
+
+    // Todo内容ダブルクリック後、情報を入力
+    const content = container.getElementsByClassName('content')
+    const grid = screen.getByRole('grid')
+
+    // 期待値確認
+    expect(axios.get).toHaveBeenCalledTimes(1)
+  });
+});
